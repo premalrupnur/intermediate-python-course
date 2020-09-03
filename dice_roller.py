5ab927b86e7537b075c09edcceb8d5ed18623d3f
premalrupnur <premalrupnur2001@gmail.com>
2020-09-03 09:15:42 +0530

    I have edited the coode

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,17 @@
 import random
 
 def main():
-  roll = random.randint(1,6)
-  print('You rolled a die',roll)
-
+  rolls = 2
+  dice_sum = 0
+  for i in range(0,rolls):
+    roll = random.randint(1,6)
+    dice_sum += roll
+    if roll == 1:
+      print("You rolled a {0}! Critical Fail".format(roll))
+    elif roll == 6:
+      print("You rolled a {0}! Critical Success!".format(roll))
+    else:
+      print("You rolled a {0}".format(roll))
+  print("You have rolled a total of {0}".format(dice_sum))
 if __name__== "__main__":
   main()
